e25d9fe1dd9422f99bca807c5995f2138f2db117
kibria <mdkibria.dev@gmail.com>
2026-09-18 17:32:41 +0600

    fix: add & remove quantity of product on cart page

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
         // Get updated cart
         const cartResponse = await fetch('/cart.js');
-        
+
         if (!cartResponse.ok) {
             throw new Error('Failed to fetch cart');
         }
         const cart = await cartResponse.json();
-        
+
         // Update cart counter
         const cartCount = document.querySelector('#cart-count');
         if (cartCount) {
@@ -17,3 +17,58 @@ document.addEventListener('DOMContentLoaded', async () => {
         console.error(error);
     }
 })
+
+
+document.addEventListener('click', async (event) => {
+    const plusButton = event.target.closest('[data-quantity-plus]');
+    const minusButton = event.target.closest('[data-quantity-minus]');
+
+    if (!plusButton && !minusButton) return;
+
+    const quantityContainer = event.target.closest('[data-cart-item]');
+    const quantityInput = quantityContainer.querySelector('[data-quantity-input]');
+
+    let quantity = parseInt(quantityInput.value, 10) || 1;
+
+    if (plusButton) {
+        quantity += 1;
+    }
+
+    if (minusButton) {
+        quantity = Math.max(1, quantity - 1);
+    }
+
+    const line = Number(quantityContainer.dataset.line);
+    console.log('Line:', line);
+    try {
+        const response = await fetch('/cart/change.js', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                line: line,
+                quantity: quantity
+            })
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update cart');
+        }
+
+        console.log('Status:', response.status);
+        console.log('OK:', response.ok);
+
+        const cart = await response.json();
+
+        console.log('Updated cart:', cart);
+
+        quantityInput.value = quantity;
+
+        console.log('Updated cart:', cart);
+
+    } catch (error) {
+        console.error(error);
+    }
+});
