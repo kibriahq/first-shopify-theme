ab81f882042c74514dc4762f240bce5cd02118e1
kibria <mdkibria.dev@gmail.com>
2026-09-22 09:14:29 +0600

    create seperate cart-drawer js file for cart drawer

---
+++ assets/cart-drawer.js
@@ -0,0 +1,81 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const cartDrawer = document.querySelector('#cart-drawer');
+    const closeButton = document.querySelector('#cart-drawer-close');
+
+    closeButton.addEventListener('click', function () {
+        cartDrawer.classList.remove('translate-x-0');
+        cartDrawer.classList.add('translate-x-full');
+    });
+
+    // Open the cart drawer when the cart icon is clicked
+    const cartIcon = document.querySelector('#cart-drawer-toggle');
+    if (cartIcon) {
+        cartIcon.addEventListener('click', function () {
+            cartDrawer.classList.remove('translate-x-full');
+            cartDrawer.classList.add('translate-x-0');
+        });
+    }
+});
+
+class CartActions extends HTMLElement {
+    constructor() {
+        super();
+
+        this.plusButton = this.querySelector('[data-plus-action]');
+        this.minusButton = this.querySelector('[data-minus-action]');
+        this.removeButton = this.querySelector('[data-remove-action]');
+    }
+
+    connectedCallback() {
+        this.plusButton.addEventListener('click', this.handlePlusClick.bind(this));
+        this.minusButton.addEventListener('click', this.handleMinusClick.bind(this));
+        this.removeButton.addEventListener('click', this.handleRemoveClick.bind(this));
+    }
+
+    handlePlusClick(event) {
+        this.#updateCart(event);
+    }
+
+    handleMinusClick(event) {
+        this.#updateCart(event);
+    }
+
+    handleRemoveClick(event) {
+        this.#updateCart(event);
+    }
+
+    async #updateCart(event) {
+        const formData = {
+            'line': this.dataset.line,
+            'quantity': event.target.dataset.quantity,
+            'sections': 'cart-drawer'
+        }
+
+        try {
+            const response = await fetch('/cart/change.js', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to update cart');
+            }
+
+            const cart = await response.json();
+
+            // Update the cart drawer with the new cart data
+            const cartElement = document.createElement('div');
+            cartElement.innerHTML = cart.sections['cart-drawer'];
+
+            document.querySelector('#shopify-section-cart-drawer').querySelector('#cart-drawer-content').innerHTML = cartElement.querySelector('#cart-drawer-content').innerHTML;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+customElements.define('cart-actions', CartActions);
